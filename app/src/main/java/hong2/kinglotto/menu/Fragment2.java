753e38f37c2550beb1d0128861fe880cc9e89510
hong-pyo <ghdvy010@gmail.com>
2020-04-23 02:37:03 +0900

    랜덤번호 뽑기 완료 and 로또번호 이미지 추가

--- app/src/main/java/hong2/kinglotto/menu/Fragment2.java
+++ app/src/main/java/hong2/kinglotto/menu/Fragment2.java
@@ -1,6 +1,93 @@
 package hong2.kinglotto.menu;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import hong2.kinglotto.R;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeSet;
 
 public class Fragment2 extends Fragment {
+    private LinearLayout extractView;
+    private ArrayList<Bitmap> lottoBall;
+    private Button extarctButton;
+
+    private Context context;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if (context != null) {
+            context = null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
+        initUI(rootView);
+        return rootView;
+    }
+
+    private void initUI(ViewGroup rootView) {
+        lottoBall = new ArrayList<>();
+        Resources res = getResources();
+
+        for(int i=0; i < 45; i++) {
+            int tempId = getResources().getIdentifier(
+                    "ball_" + (i+1), "drawable", getActivity().getPackageName()
+            );
+            Bitmap bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(res, tempId) ,150,150, false);
+            lottoBall.add(bitmap);
+        }
+        final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        extractView = rootView.findViewById(R.id.extractView);
+        extractView.setLayoutParams(params);
+        extractView.setOrientation(LinearLayout.HORIZONTAL);
+        extarctButton = rootView.findViewById(R.id.extractButton);
+
+        extarctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // add View 로 뷰를 추가하기 떄문에 이전뷰는 삭제. (이전에 뽑은 애들) API 쓰면 필요없을듯..? 잘해결해보자.
+                if(extractView.getChildCount() > 0) {
+                    extractView.removeAllViews();
+                }
+
+                TreeSet<Integer> set = new TreeSet<>();
+                while (set.size() < 6) {
+                    int random = new Random().nextInt(45) + 1;
+                    set.add(random);
+                }
+
+                for (Integer i : set) {
+                    // 이미지 생성
+                    ImageView imageView = new ImageView(extractView.getContext());
+                    // 셋팅
+                    imageView.setImageBitmap(lottoBall.get(i-1));
+                    extractView.addView(imageView);
+                }
+            }
+        });
+    }
 }
