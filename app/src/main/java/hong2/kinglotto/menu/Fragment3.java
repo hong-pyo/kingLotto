bde721700f32ee76ed2a3ced9077b69cf2b3b8a4
hong-pyo <ghdvy010@gmail.com>
2020-05-06 01:00:46 +0900

    fragment3 setting button

--- app/src/main/java/hong2/kinglotto/menu/Fragment3.java
+++ app/src/main/java/hong2/kinglotto/menu/Fragment3.java
@@ -1,6 +1,46 @@
 package hong2.kinglotto.menu;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import hong2.kinglotto.R;
 
 public class Fragment3 extends Fragment {
+    private Button logoutButton;
+    private Context context;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if (context != null) {
+            context = null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rooeView = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+        initUI(rooeView);
+
+        return rooeView;
+    }
+
+    private void initUI(ViewGroup rooeView) {
+        logoutButton = rooeView.findViewById(R.id.kakao_logout);
+
+
+    }
 }
